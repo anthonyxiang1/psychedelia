3b136f01e4173c8e82a27ec07f4f5603f4712780
axian0420 <anthony0xiang@gmail.com>
2020-04-29 15:55:17 -0400

    1.2

--- config.py
+++ config.py
@@ -9,3 +9,16 @@ S3_LOCATION = 'http://{}.s3.amazonaws.com/'.format('psychedeliafinal')
 # tensorflow==1.13.0rc1
 # tensorflow-estimator==2.0.1
 # tensorflow-hub==0.2.0
+
+# jupyter==1.0.0
+# jupyter-client==5.2.4
+# jupyter-console==6.0.0
+# jupyter-core==4.4.0
+# jupyterlab==0.35.4
+# jupyterlab-server==0.2.0
+# Keras==2.2.4
+# Keras-Applications==1.0.7
+# Keras-Preprocessing==1.0.9
+# opencv-python==4.0.0.21
+# pygame==1.9.6
+# twilio==6.30.0
