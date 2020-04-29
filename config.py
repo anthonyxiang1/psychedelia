1a19e67ec76ad01c2f27ae8b722757722b3ace3d
axian0420 <anthony0xiang@gmail.com>
2020-04-29 15:33:11 -0400

    too large

--- config.py
+++ config.py
@@ -5,3 +5,7 @@ S3_LOCATION = 'http://{}.s3.amazonaws.com/'.format('psychedeliafinal')
 
 #torch==1.4.0+cpu
 #torchvision==0.5.0+cpu
+
+# tensorflow==1.13.0rc1
+# tensorflow-estimator==2.0.1
+# tensorflow-hub==0.2.0
