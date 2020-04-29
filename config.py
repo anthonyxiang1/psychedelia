708b312d6b8c5af6b5c7d061e185f9fd860a7f08
axian0420 <anthony0xiang@gmail.com>
2020-04-29 15:20:56 -0400

    no torch

--- config.py
+++ config.py
@@ -2,3 +2,5 @@ import os
 ACCESS_KEY =  'AKIA5SRTJDNAZ7HGU47D'
 SECRET_KEY = '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'
 S3_LOCATION = 'http://{}.s3.amazonaws.com/'.format('psychedeliafinal')
+
+#torch==1.4.0+cpu
